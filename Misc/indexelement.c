9124d75e65a2ff0916471ab46d8a6a314985ac7b
Dhrumil Patel <dhrumilp15@gmail.com>
2018-05-27 16:44:01 -0400

    Create indexelement.c

---
+++ Misc/indexelement.c
@@ -0,0 +1,43 @@
+//Outputs an array of length given as a commandline argument (w/out cs50.h)
+
+//Get libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    //Only want 1 command line argument
+    if (!(argc == 2))
+    {
+        return 1;
+    }
+
+    int length = 0;
+    //The given length must be an integer and grater than 0
+    if (atoi(argv[1]) > 0)
+    {
+        length = atoi(argv[1]);
+    }
+    else
+    {
+        return 1;
+    }
+
+    //Init array
+    int array[length];
+
+    //Formatting to look like an array
+    printf("{");
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = i;
+        printf("%d", array[i]);
+        //Print a comma unless at the last element
+        if (i != length - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("}\n");
+    return 0;
+}
